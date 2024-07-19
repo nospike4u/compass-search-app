bd5e9aa6bf54c80594a16638aa61ee10350f60fc
Gary February <gfebruary@gmail.com>
2024-07-19 22:18:58 +0200

    footer createde

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,26 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Login from './Pages/Login.jsx';
-import Navbar from './Components/Navbar.jsx';
-import Home from './Pages/Home.jsx';
-import Footer from './Components/Footer.jsx';
-import Documentation from './Pages/Documentation.jsx';
-import EditProfile from './Pages/EditProfile.jsx';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login.jsx";
+import Navbar from "./Components/Navbar.jsx";
+import Home from "./Pages/Home.jsx";
+import Footer from "./Components/Footer.jsx";
+import Documentation from "./Pages/Documentation.jsx";
+import EditProfile from "./Pages/EditProfile.jsx";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        ></Route>
-        <Route
-          path="/login"
-          element={<Login />}
-        ></Route>
-        <Route
-          path="/documentation"
-          element={<Documentation />}
-        ></Route>
-        <Route
-          path="/editprofile"
-          element={<EditProfile />}
-        ></Route>
-      </Routes>
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/documentation" element={<Documentation />}></Route>
+          <Route path="/editprofile" element={<EditProfile />}></Route>
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
