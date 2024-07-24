0616701e15c40ae2c618de5cde28db9119e04f8b
nospike4u <nospike4u@hotmail.com>
2024-07-24 18:00:26 +0200

    integrating visual! with rest of site

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import SearchBar from "../Components/Searchbar";
 import FloatingBox from "../Components/FloatingBox";
 import ContextMenu from "../Components/ContextMenu";
+import TheMagic from "../Components/TheMagic";
 
 const Home = () => {
   return (
     <div>
       <SearchBar />
+      <TheMagic />
       {/* <FloatingBox /> */}
       <ContextMenu />
     </div>
